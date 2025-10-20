20fbaaf6b4c1f8b65868031a88a498cc431f3993
FriendlyGhost <ffriendlygghostt@gmail.com>
2025-10-20 19:19:02 +0500

    EnemyType enum

---
+++ Assets/Scripts/Enemy/EnemyType.cs
@@ -0,0 +1,19 @@
+
+public enum EnemyType
+{
+    Rot,
+    Wolf,
+    Slime_Green,
+    Slime_Blue,
+    Slime_Fire,
+    Plant1,
+    Plant2,
+    Plant3,
+    Goblin1,
+    Goblin2,
+    Goblin3,
+    Orc,
+    Mage,
+    HorseRider,
+    BeholderBoss
+}
