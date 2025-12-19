cea282f1052e86152cf0e866e179e8c4d3f3e34e
FriendlyGhost <ffriendlygghostt@gmail.com>
2025-12-20 01:14:55 +0500

    EnemyHealthBar | DarkHitFlash | FadeOutDeath | Upgrade_Enemy_Sripts

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -6,27 +7,54 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private EnemyStats stats;
     [SerializeField] private EnemyMovement movement;
     [SerializeField] private EnemyAnimatorController animationController;
+    [SerializeField] private EnemyHealthBar enemyHealthBar;
 
     public EnemyType Type => stats.type;
 
     public event Action<float> OnSpeedChanged;
+
+    [ContextMenu("DEBUG / Deal30Damage")]
+    private void DebugDamage()
+    {
+        TakeDamage(30f);
+    }
+
     private void Awake()
     {
         stats = GetComponent<EnemyStats>();
         movement = GetComponent<EnemyMovement>();
         animationController = GetComponent<EnemyAnimatorController>();
+        enemyHealthBar = GetComponent<EnemyHealthBar>();
 
-        if (stats == null || movement == null || animationController == null)
+        if (stats == null || movement == null || animationController == null
+            || enemyHealthBar == null)
         {
-            Debug.LogWarning("Enemy has not: EnemyStats | EnemyMovement | EnemyAnimation");
+            Debug.LogWarning("Enemy has not: EnemyStats | EnemyMovement | EnemyAnimation | EnemyHealthBar");
         }
+    }
+
+    private void OnEnable()
+    {
+        stats.ResetStats();
+        movement.ResetMovement();
+        animationController.ResetAnimation();
+        gameObject.SetActive(true);
+        enemyHealthBar.Init(stats.maxHealth);
+        enemyHealthBar.Show();
+
+        stats.OnDeathAction += Die;
+    }
+
+    private void OnDisable()
+    {
+        stats.OnDeathAction -= Die;
     }
 
     public void Die()
     {
         movement.StopMovement();
         animationController.PlayDeath(movement.GetCurrentDirection());
-        
+        enemyHealthBar.Hide();
     }
 
     public float GetMoveSpeed() => stats.moveSpeed;
@@ -40,16 +68,15 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    public void TakeDamage(float damage)
+    {
+        stats.TakeDamage(damage);
+        enemyHealthBar.UpdateHealth(damage);
+        animationController.PlayHitFlash();
+    }
+
     public void PlayWalk(EnemyDirection direction)
     {
         animationController.PlayWalk(direction);
-    }
-
-    public void ResetState()
-    {
-        stats.ResetStats();
-        movement.ResetMovement();
-        animationController.ResetAnimation();
-        gameObject.SetActive(true);
     }
 }
