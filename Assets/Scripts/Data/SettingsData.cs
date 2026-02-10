0019f371b62afd4203cb77890eaa5d5086b0146b
ffriendlygghostt <ffriendlygghostt.gmail.com>
2026-02-10 22:57:43 +0500

    SettingsMenu

--- Assets/Scripts/Data/SettingsData.cs
+++ Assets/Scripts/Data/SettingsData.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 [System.Serializable]
 public class SettingsData
 {
-    
+    public float musicVolume = 1f;
+    public float sfxVolume = 1f;
+    public bool isFullScreen = true;
+    public int resolutionIndex = 0;
 }
